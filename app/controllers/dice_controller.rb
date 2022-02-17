1692d75d61c525f65e683edfb0b44f9be5b92167
ligeoff1 <geoff.li@kellogg.northwestern.edu>
2022-02-17 01:50:35 +0000

    update with GET

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,3 +1,8 @@
 class DiceController < ApplicationController
-
+    def index #called an action 
+        @die1 = rand(1..6)
+        @die2 = rand(1..6)
+        @total = @die1 + @die2
+        @name = params["name"] #give you access to whatever is the in the parameters - see terminal after search for something after ? 
+    end 
 end
