34a146df2e1965e02c070ed343e727ed92ee4424
ligeoff1 <geoff.li@kellogg.northwestern.edu>
2022-02-17 03:28:59 +0000

    post class update

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,4 +4,15 @@ class ContactsController < ApplicationController
     @contact = Contact.find(params["id"])
   end
 
+  def new
+    @contact = Contact.new
+    @contact.company_id = params["company_id"]
+  end
+
+  def create  
+    @contact = Contact.new(params["contact"])
+    @contact.save
+    redirect_to "/companies/#{@contacts.company_id}"
+  end
+
 end
